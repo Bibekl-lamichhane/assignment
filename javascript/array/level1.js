e020b9377f80fecd98e81fd4c4c557e8e9a48146
Bibekl-lamichhane <bibek1999lamichhane@gmail.com>
2024-04-23 09:19:20 +0545

    level1.js completed

---
+++ javascript/array/level1.js
@@ -0,0 +1,15 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+console.log(arr.length)
+
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+const arrRev =arr.reverse()
+console.log(arrRev)
+
+///Q3 join the array and console log
+// expected output 
+console.log(arr.join(""))
