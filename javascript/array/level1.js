31eaac7a97580a0460523a8d3723478983d41ec1
Bibekl-lamichhane <bibek1999lamichhane@gmail.com>
2024-04-25 20:38:50 +0545

    level1.js and level2.js  added completed

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -11,5 +11,32 @@ const arrRev =arr.reverse()
 console.log(arrRev)
 
 ///Q3 join the array and console log
-// expected output 
-console.log(arr.join(""))
+// expected output 2,5,3
+console.log(arr.join())
+
+//Q4 Remove the last element from the array and log the modified array to the console
+
+const color = ['blue', 'Purple', 'red', 'black', 'white'];
+
+console.log(color.pop());
+// ans = white
+console.log(color);
+// ans= [ 'blue', 'Purple', 'red', 'black' ]
+
+
+
+// Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,4,8,10,12]
+
+if (num/2){
+  console.log('All Even');
+}else{
+  console.log('Not all even');
+}
+//ans = not all even
+
+//Q6 Find the index of a specific element in the array. Log the index to the console. 
+const paint = ['blue', 'Purple', 'red', 'black', 'white'];
+
+console.log(paint.indexOf('Purple'));
+//and =1
